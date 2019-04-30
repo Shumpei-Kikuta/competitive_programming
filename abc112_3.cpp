020b41efc1de1cd9beab8217977bb85b6673966c
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-04-30 11:42:26 +0900

    why it's wrong

---
+++ abc112_3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int X[101], Y[101];
+long int H[101];
+int Cx, Cy;
+long int ans;
+
+void check(){
+    for (int i = 0; i <= 100; i++){
+        for (int j = 0; j <= 100; j++){
+            bool flag = true;
+            for (int k = 0; k < N; k++){
+                if (H[k] == 0){
+                    continue;
+                }else{
+                    if ((k == 0) || (((ans == abs(X[k] - i) + abs(Y[k] - j) + H[k])) && (ans >= 1))){
+                        ans = abs(X[k] - i) + abs(Y[k] - j) + H[k];
+                    }else{
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if (flag){
+                Cx = i;
+                Cy = j;
+                return;
+            }
+        }
+    }
+    return;
+}
+
+
+int main(){
+    cin >> N;
+    for (int i = 0; i < N; i ++){
+        cin >> X[i] >> Y[i] >> H[i];
+    }
+    check();
+    cout << Cx << " " << Cy << " " << ans << endl;
+
+    return 0;
+}
