eef619b36b426e06cdbd6275432fde580e721141
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-05-03 16:23:26 +0900

    complete

--- abc096_3.cpp
+++ abc096_3.cpp
@@ -7,6 +7,35 @@ using namespace std;
 int H;
 int W;
 int matrix[52][52] = {};
+
+bool dfs(int now_y, int now_x){
+    // 隣がいればfalse, いなければtrue
+    for (int i = -1; i <= 1; i++){
+        for (int j = -1; j <= 1; j++){
+            if ((i + j) % 2 != 0){
+                if (matrix[now_y + i][now_x + j] == sharp){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool is_solve(){
+    for (int i = 1; i <= H; i++){
+        for (int j = 1; j <= W; j++){
+            if (matrix[i][j] == sharp){
+                if (dfs(i, j)){
+                    // 隣がいなかったとき
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
 int main(){
     cin >> H >> W;
     for (int i = 0; i <= H; i++){
@@ -14,15 +43,33 @@ int main(){
             if ((i == 0) || (j == 0)){
                 matrix[i][j] = 0;
             }else{
-                cin >> matrix[i][j];
+                char str;
+                cin >> str;
+                if (str == '#'){
+                    matrix[i][j] = sharp;
+                }else{
+                    matrix[i][j] = dot;
+                }
             }
         }
     }
-    for (int i = 0; i <= H; i++){
-        for (int j = 0; j <= W; j++){
-            cout << matrix[i][j]<< " ";
-        }
-        cout << endl;
+    // for (int i = 0; i <= H + 1; i++){
+    //     for (int j = 0; j <= W + 1; j++){
+    //         cout << matrix[i][j]<< " ";
+    //     }
+    //     cout << endl;
+    // }
+    if (is_solve()){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
     }
+    // for (int i = 0; i <= H + 1; i++){
+    //     for (int j = 0; j <= W + 1; j++){
+    //         cout << matrix[i][j]<< " ";
+    //     }
+    //     cout << endl;
+    // }
+
     return 0;
 }
