e4b13efbc32e02db5e4199932abcd2ba0944297d
Shumpei-Kikuta <shunpei-kikuta775@g.ecc.u-tokyo.ac.jp>
2019-11-01 08:21:10 +0900

    format

--- abc038_c.py
+++ abc038_c.py
@@ -5,22 +5,22 @@ sys.setrecursionlimit(10000000)
 def main():
     N = int(input())
     A = [int(c) for c in input().split()]
-    l = 0
-    r = 0
+    left = 0
+    right = 0
     num = 0
-    while(r < N):
-        while(r + 1 < N):
-            if A[r + 1] > A[r]:
-                r += 1
+    while(right < N):
+        while(right + 1 < N):
+            if A[right + 1] > A[right]:
+                right += 1
             else:
                 break
-        for i in range(1, r - l + 1):
+        for i in range(1, right - left + 1):
             num += i
-        r += 1
-        l = r
+        right += 1
+        left = right
     num += N
     print(num)
-            
+
 
 if __name__ == '__main__':
     main()
